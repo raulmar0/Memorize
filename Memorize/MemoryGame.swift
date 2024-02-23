ca8534892afedf93e83ac6aff3a453dcf9cf82cf
raulmar <raulhdz40@gmail.com>
2024-02-22 20:27:31 -0600

    cleanup after lecture 5

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -22,30 +22,6 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
         }
     }
     
-//    var indexOfTheOneAndOnlyFaceUpCard: Int? {
-//        get {
-//            var faceUpCardIndices = [Int]()
-//            for index in cards.indices {
-//                if cards[index].isFaceUp {
-//                    faceUpCardIndices.append(index)
-//                }
-//            }
-//            if faceUpCardIndices.count == 1 {
-//                return faceUpCardIndices.first
-//            } else {
-//                return nil
-//            }
-//        }
-//        set {
-//            for index in cards.indices {
-//                if index == newValue {
-//                    cards[index].isFaceUp = true
-//                } else {
-//                    cards[index].isFaceUp = false
-//                }
-//            }
-//        }
-//    }
     
     var indexOfTheOneAndOnlyFaceUpCard: Int? {
         get { return cards.indices.filter { index in cards[index].isFaceUp }.only }
@@ -55,7 +31,6 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     mutating func choose(_ card: Card) {
         print("chose \(card)")
         if let chosenIndex = cards.firstIndex(where: {$0.id == card.id}) {
-//        if let chosenIndex = index(of: card) {
             if !cards[chosenIndex].isFaceUp && !cards[chosenIndex].isMatched {
                 if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
                     if cards[chosenIndex].content == cards[potentialMatchIndex].content {
@@ -69,16 +44,7 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
             }
         }
     }
-    
-    private func index(of card: Card) -> Int? {
-        for index in cards.indices {
-            if cards[index].id == card.id {
-                return index
-            }
-        }
-        return nil // FIXME: bogus!
-    }
-    
+        
     mutating func shuffle() {
         cards.shuffle()
         print(cards)
